cf51aeaafa47b4f6273e516156d4dfb99320699a
Lesedi <skhosanalesedi888@gmail.com>
2024-09-27 12:54:45 +0200

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Home } from './pages/Home'
+import Register from './pages/Register'
+
 
 function App() {
   return (
-  <div>Hello</div>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
+
+    </Routes>
+  </BrowserRouter>
   )
 }
 
